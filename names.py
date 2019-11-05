a2abea368b66882ccd3d77ba98009acada72a451
Sudharshannalla <nallareddys2@gmail.com>
2019-11-05 17:20:45 +0530

    orderer

--- names.py
+++ names.py
@@ -2,4 +2,9 @@ ac=input('enter your name:')
 if ac == "nalla":
     print("sss")
 else:
+    nallas = input('enter order')
+    if nallas== 'two':
+        print('two')
+    else:
+        print('one')
     print("nnn")
